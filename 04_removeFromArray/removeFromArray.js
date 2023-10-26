7cb8b2332a1b4f97d472e9ef9099264b812f697c
kimaru355 <emmanuelkimaru355@gmail.com>
2023-10-26 22:59:23 +0300

    remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (...args) {
+  let myArray = args[0];
+  args.shift();
+  for (let i = 0; i < args.length; i++) {
+    for (let j = 0; j < myArray.length; j++) {
+      if (args[i] === myArray[j]) {
+        myArray.splice(j, 1);
+      }
+    }
+  }
+  return myArray;
 };
 
 // Do not edit below this line
