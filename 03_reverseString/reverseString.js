dfb8e878124d183e71b985e658120cd09d0bca9d
kimaru355 <emmanuelkimaru355@gmail.com>
2023-10-26 23:28:19 +0300

    sum numbers between two numbers

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,7 @@
 const reverseString = function (myString) {
   let myArray = Array.from(myString);
-  let arrayLength = myArray.length;
   let reversedArray = [];
-  for (let i = 0; i < arrayLength; i++) {
+  for (let i = 0; i < myArray.length; i++) {
     reversedArray.unshift(myArray[i]);
   }
   let reversedString = reversedArray.join("");
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (...args) {
+  if (args.length != 2) {
+    return "ERROR";
+  }
+  if (typeof args[0] != "number" || typeof args[1] != "number") {
+    return "ERROR";
+  }
+  let sum = 0;
+  let largerNum;
+  if (args[0] < 0 || args[1] < 0) {
+    return "ERROR";
+  } else if (args[0] > args[1]) {
+    largerNum = args[0];
+    args.shift();
+  } else {
+    largerNum = args[1];
+    args.pop();
+  }
+  for (let num = args[0]; num <= largerNum; num++) {
+    sum += num;
+  }
+  return sum;
 };
 
 // Do not edit below this line
