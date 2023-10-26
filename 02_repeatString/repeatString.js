ce2169cfe6db4a6cddb9f72daeef192aa2b93c58
kimaru355 <emmanuelkimaru355@gmail.com>
2023-10-26 22:15:05 +0300

    array methods and jest

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (myString, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+  let fullString = "";
+  for (let i = 0; i < num; i++) {
+    fullString += myString;
+  }
+  return fullString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (myString) {
+  let myArray = Array.from(myString);
+  let arrayLength = myArray.length;
+  let reversedArray = [];
+  for (let i = 0; i < arrayLength; i++) {
+    reversedArray.unshift(myArray[i]);
+  }
+  let reversedString = reversedArray.join("");
+  return reversedString;
 };
 
 // Do not edit below this line
