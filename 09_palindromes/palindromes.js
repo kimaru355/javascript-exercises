e6676cbc253e40f0798e97b2a04502ac150477d4
kimaru355 <emmanuelkimaru355@gmail.com>
2023-10-30 12:01:04 +0300

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  word = word.split("");
+  let regex = /^[a-zA-Z0-9]$/;
+  word = word.filter((letter) => regex.test(letter));
+  for (let i = 0; i <= word.length / 2; i++) {
+    if (word[i].toLowerCase() === word[word.length - (i + 1)].toLowerCase()) {
+      // do nothing if same
+    } else {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
