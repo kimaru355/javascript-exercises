b8a0fb1436aeae1cdda1df316f1ebef36397c509
kimaru355 <emmanuelkimaru355@gmail.com>
2023-10-31 20:35:01 +0300

    fibonacci numbers and objects

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (typeof num === "string") {
+    num = parseInt(num, 10);
+  }
+  if (num < 1) {
+    return "OOPS";
+  }
+  let fib1 = 1;
+  let fib2 = 1;
+  let fib3;
+  let counter = 1;
+  while (true) {
+    if (num === counter) {
+      return fib1;
+    }
+    fib3 = fib1;
+    fib1 = fib2;
+    fib2 += fib3;
+    counter += 1;
+  }
 };
 
 // Do not edit below this line
