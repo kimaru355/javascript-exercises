11be17fb3e4025c9e59c12fe0641ed98e8f7a9a7
kimaru355 <emmanuelkimaru355@gmail.com>
2023-10-31 23:56:35 +0300

    find the oldest person in an array of objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const date = new Date();
+  let updatedPeople = people.map((person) => {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = date.getFullYear();
+      return person;
+    } else {
+      return person;
+    }
+  });
+  let sortedPeople = updatedPeople.sort((person1, person2) =>
+    person1.yearOfDeath - person1.yearOfBirth >
+    person2.yearOfDeath - person2.yearOfBirth
+      ? -1
+      : 1
+  );
+  return sortedPeople[0];
 };
 
 // Do not edit below this line
